f56d13e5c2422c49291f107ccba21a572220d3d3
김민수 <alstn2468_@naver.com>
2019-09-06 14:57:02 +0900

    Define some colors in Constatnts/Colors.js

---
+++ phoenixia/src/Constatnts/Colors.js
@@ -0,0 +1,11 @@
+export default BACKGROUND_COLOR = "#000000";
+export default BACKGROUND_COLOR_900 = "#263238";
+export default BACKGROUND_COLOR_800 = "#37474F";
+export default BACKGROUND_COLOR_700 = "#455A64";
+export default BACKGROUND_COLOR_600 = "#546E7A";
+export default TINT_COLOR = "#FFFFFF";
+export default SCH_LOGO_COLOR_ONE = "#AAD047";
+export default SCH_LOGO_COLOR_TWO = "#007A85";
+export default SCH_LOGO_COLOR_THR = "#1BBCF0";
+export default ACTIVE_COLOR = "#FFFFFF";
+export default DEACTIVE_COLOR = "#000000";
