cb4f05af1707b166183f4c1fd76d5a172be8eaa9
김민수 <alstn2468_@naver.com>
2019-09-12 18:32:02 +0900

    Add polyfill App.js to index.js

--- phoenixia/src/Components/App.js
+++ phoenixia/src/Components/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyles from "Components/GlobalStyles";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/ie9";
 
 function App() {
     return (
--- phoenixia/src/index.js
+++ phoenixia/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Components/App";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/ie9";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
