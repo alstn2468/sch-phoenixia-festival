e83ddde02239a19063d8a9fc9be63151312c9a16
김민수 <alstn2468_@naver.com>
2019-09-12 17:56:52 +0900

    Add polyfill

--- phoenixia/src/Components/App.js
+++ phoenixia/src/Components/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyles from "Components/GlobalStyles";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/ie9";
+import "../../index.css";
 
 function App() {
     return (
