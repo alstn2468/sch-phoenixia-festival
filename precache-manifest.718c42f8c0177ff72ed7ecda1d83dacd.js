b6f42bf90553fa38cba5979ac4acd3a396c53922
김민수 <alstn2468_@naver.com>
2019-09-12 12:01:01 +0900

    Updates

---
+++ precache-manifest.718c42f8c0177ff72ed7ecda1d83dacd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dc15117f3ba7218c765678dcb33891",
+    "url": "/SCH_Phoenixia_Festival/index.html"
+  },
+  {
+    "revision": "99f84c3b44e73d876426",
+    "url": "/SCH_Phoenixia_Festival/static/js/2.640fbd25.chunk.js"
+  },
+  {
+    "revision": "9fb33b6670189dc0e1a5",
+    "url": "/SCH_Phoenixia_Festival/static/js/main.01e30857.chunk.js"
+  },
+  {
+    "revision": "74b5e600afc236af6cd1",
+    "url": "/SCH_Phoenixia_Festival/static/js/runtime~main.98e31661.js"
+  },
+  {
+    "revision": "8ccd88b4f9f4f64158ec9d4626e3daee",
+    "url": "/SCH_Phoenixia_Festival/static/media/BOL4.8ccd88b4.jpeg"
+  },
+  {
+    "revision": "1642af2e7e35f2fd40a669a03e4f6471",
+    "url": "/SCH_Phoenixia_Festival/static/media/soonchunhyang.1642af2e.ttf"
+  }
+]);
