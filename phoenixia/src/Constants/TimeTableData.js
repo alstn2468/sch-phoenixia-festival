cbb3c588bd28d96384b1107fa6f1c7b3d2d5e0c2
김민수 <alstn2468_@naver.com>
2019-09-15 22:48:59 +0900

    Fix some styles

--- phoenixia/src/Constants/TimeTableData.js
+++ phoenixia/src/Constants/TimeTableData.js
@@ -1,35 +1,35 @@
 export const DAY_ONE_TIMETABLE = [
     { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" }
+    { time: "10:00 ~ 11:00", content: "Test1" },
+    { time: "11:00 ~ 12:00", content: "Test1" },
+    { time: "12:00 ~ 13:00", content: "Test1" },
+    { time: "13:00 ~ 14:00", content: "Test1" },
+    { time: "14:00 ~ 16:00", content: "Test1" },
+    { time: "16:00 ~ 18:00", content: "Test1" },
+    { time: "18:00 ~ 19:00", content: "Test1" },
+    { time: "19:00 ~ 20:00", content: "Test1" }
 ];
 
 export const DAY_TWO_TIMETABLE = [
     { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" }
+    { time: "10:00 ~ 11:00", content: "Test1" },
+    { time: "11:00 ~ 12:00", content: "Test1" },
+    { time: "12:00 ~ 13:00", content: "Test1" },
+    { time: "13:00 ~ 14:00", content: "Test1" },
+    { time: "14:00 ~ 16:00", content: "Test1" },
+    { time: "16:00 ~ 18:00", content: "Test1" },
+    { time: "18:00 ~ 19:00", content: "Test1" },
+    { time: "19:00 ~ 20:00", content: "Test1" }
 ];
 
 export const DAY_THR_TIMETABLE = [
     { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" },
-    { time: "08:00 ~ 10:00", content: "Test1" }
+    { time: "10:00 ~ 11:00", content: "Test1" },
+    { time: "11:00 ~ 12:00", content: "Test1" },
+    { time: "12:00 ~ 13:00", content: "Test1" },
+    { time: "13:00 ~ 14:00", content: "Test1" },
+    { time: "14:00 ~ 16:00", content: "Test1" },
+    { time: "16:00 ~ 18:00", content: "Test1" },
+    { time: "18:00 ~ 19:00", content: "Test1" },
+    { time: "19:00 ~ 20:00", content: "Test1" }
 ];
